338a89b11c34ed5983968202900b429978271fa3
Ajay Ganapathy <ajay@incremental.design>
2026-07-25 20:44:56 -0400

    feat: install script subcommands
    four subcommands: system, uninstall-system
    home, uninstall-home
    
    each subcommand has a subcommand of help
    
    nix run infrastructure/.#install -- system help
    
    nix run infrastructure/.#install -- home help
    
    nix run infrastructure/.#install -- uninstall-system help
    
    nix run infrastructure/.#install -- uninstall-home help
    
    Signed-off-by: Ajay Ganapathy <ajay@incremental.design>

--- infrastructure/install.nix
+++ infrastructure/install.nix
@@ -9,7 +9,7 @@ pkgs.writeShellApplication {
       on MacOS:
          ___________
         /           |
-       /   incremental-design/projects?dir=infrastructure#install
+       /   incremental-design/projects?dir=infrastructure#install -- system
        |            |
        |            |                 /
        |            |                  |- Applications/ <---------------------------------,
@@ -18,7 +18,7 @@ pkgs.writeShellApplication {
              |                         |                                                  |
              |                         |- System/                                         |
              |                         |                                                  |
-             |                         |- Users/        <---------------------------------|
+             |                         |- Users/                                          |
              |                         |                                                  |
              |                         |- Volumes/                                        |
              |                         |                                                  |
@@ -62,261 +62,257 @@ pkgs.writeShellApplication {
 
       1. initializes a flake.nix with overrides from CLI input into /var/root
       2. runs darwin-rebuild switch to build and apply patches to MacOS system folders
+      on MacOS:
+         ___________
+        /           |
+       /   incremental-design/projects?dir=infrastructure#install -- user
+       |            |
+       |            |                 /
+       |            |                  |- Applications/
+       |            |                  |
+       |_____,______|                  |- Library/
+             |                         |
+             |                         |- System/
+             |                         |
+             |                         |- Users/
+             |                         |    |
+             |                         |    |- <username>/
+             |                         |    :    |
+             |                         |         |
+             '------------(1)--------------->    '- flake.nix
+                                       :
+                                       :
+
+
+
+      1. initializes a flake.nix with overrides from CLI input into /Users/<username>
+      2. runs home-manager/release-26.05 -- init --switch to build and apply home directory configuration
 
       on NixOS
 
       nothing yet - not implemented
     '';
   };
-  runtimeInputs = [
-    pkgs.coreutils
-    pkgs.gnused
+  runtimeInputs = with pkgs; [
+    coreutils
+    gnused
+    glow
   ];
+
   text = ''
     set -e
 
-    if [ "$EUID" -ne 0 ]; then
-      echo "run this script with sudo -H" >&2
-      exit 1
-    fi
-
+    SUBCOMMAND=
+    HELPMSG=0
+    EXIT_CODE=0
+    SYSTEM_HOSTNAME=""
     FLAG=""
 
-    LEN_ARGS="$#"
-    HOSTNAME=""
-    UNINSTALL=""
-    HELP=""
+    if (( $# == 0 )); then
+        HELPMSG=3
+    fi
 
-    set_flag(){
-        if [ -n "$FLAG" ]; then
-            echo "$FLAG not followed by argument, got \"$1\"" >&2
-            exit 1
+    while (( $# > 0 )); do
+        if [ -z "$SUBCOMMAND" ] && [ "''${1,,}" = "system" ] || [ "''${1,,}" = "home" ] || [ "''${1,,}" = "uninstall-system" ] || [ "''${1,,}" = "uninstall-home" ]; then
+            SUBCOMMAND="$1"
+            shift
+        elif [ -z "$SUBCOMMAND" ] && [ "''${1,,}" = "help" ]; then
+            HELPMSG=3
+            break;
+        elif [ "$SUBCOMMAND" = "system" ] && [ -z "$FLAG" ] && [ "$1" = "help" ]; then
+            HELPMSG=1
+            break
+        elif [ "$SUBCOMMAND" = "system" ] && [ -z "$FLAG" ] && [ "$1" = "--hostname" ]; then
+            FLAG="--hostname"
+            shift
+        elif [ "$SUBCOMMAND" = "system" ] && [ -z "$FLAG" ] && [[ "$1" = --hostname=* ]]; then
+            SYSTEM_HOSTNAME="''${1#--hostname=}"
+            shift
+        elif [ "$SUBCOMMAND" = "system" ] && [ "$FLAG" = "--hostname" ]; then
+            FLAG=""
+            SYSTEM_HOSTNAME="$1"
+            shift
+        elif [ "$SUBCOMMAND" = "home" ] && [ -z "$FLAG" ] && [ "$1" = "help" ]; then
+            HELPMSG=2
+            break
+        elif [ "$SUBCOMMAND" = "uninstall-system" ] && [ -z $FLAG ] && [ "$1" = "help" ]; then
+            HELPMSG=4
+            break
+        elif [ "$SUBCOMMAND" = "uninstall-home" ] && [ -z $FLAG ] && [ "$1" = "help" ]; then
+            HELPMSG=5
+            break
         else
-            FLAG="$1"
+            echo "unrecognized argument ''${1}" >&2
+            HELPMSG=3
+            EXIT_CODE=1
+            break
         fi
-    }
-
-    set_arg(){
-        case "$FLAG" in
-            "")
-              echo "\"$1\" not preceded by any flag" >&2
-              exit 1
-            ;;
-            "--hostname")
-              HOSTNAME="$1";
-            ;;
-            "--uninstall"|"-u")
-              UNINSTALL="$1";
-            ;;
-            "--help"|"-h")
-              HELP="$1";
-            ;;
-        esac
-        FLAG=""
-    }
-
-    # we have to use a bare while loop for arg and flag parsing because otherwise $# $@ $1 refers to function args not script args
-    while [ "$#" -gt 0 ]; do
-
-      case "$1" in
-          "--hostname")
-              set_flag "$1"
-              shift
-          ;;
-          "--uninstall"|"-u"|"--help"|"-h")
-              # uninstall and help are special because they are flags with no args following them
-              set_flag "$1"
-              set_arg "$1"
-              shift
-          ;;
-          *)
-              set_arg "$1"
-              shift
-          ;;
-      esac
     done
 
-    # uninstall should not be passed with other args
-    if [ -n "$UNINSTALL" ] && [ "$LEN_ARGS" -gt 1 ]; then
-        echo "cannot pass \"$UNINSTALL\" with other args. run install --help for usage" >&2
-        exit 1
+    if [ "$SUBCOMMAND" = "system" ] && [ -t 0 ] && (( HELPMSG == 0 )); then
+
+    SYSTEM_HOSTNAME_RE='^([A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*\.?$'
+
+        while ! [[ "$SYSTEM_HOSTNAME" =~ $SYSTEM_HOSTNAME_RE ]] ||
+        [ "''${#SYSTEM_HOSTNAME}" -gt 255 ]; do
+            echo "\"$SYSTEM_HOSTNAME\" is not a valid RFC 1123 hostname: Enter hostname:" >&2
+            IFS= read -r SYSTEM_HOSTNAME
+        done
+
+    elif [ "$SUBCOMMAND" = "system" ] && (( HELPMSG == 0 )); then
+
+        if ! [[ "$SYSTEM_HOSTNAME" =~ $SYSTEM_HOSTNAME_RE ]] ||
+            [ "''${#SYSTEM_HOSTNAME}" -gt 255 ]; then
+            echo "\"$SYSTEM_HOSTNAME\" is not a valid RFC 1123 hostname" >&2
+            HELPMSG=1
+            EXIT_CODE=1
+        fi
+
+    # elif [ "$SUBCOMMAND" = "user" ] && [ -t 0 ] && (( HELPMSG == 0 )); then
+    # elif [ "$SUBCOMMAND" = "user" ] && (( HELPMSG == 0 )); then
     fi
 
-    if [ -n "$HELP" ]; then
-    cat >&2 <<EOF
-    Usage:
+    if (( HELPMSG == 1 )) || (( HELPMSG == 3 )); then
+    glow >&2 <<'EOF'
+    # INSTALL NixOS or nix-darwin on MacOS
+
+    ```
     sudo -H nix run \
-    --extra-experimental-features "nix-command flakes" \
-    incremental-design/projects?dir=infrastructure#install \
-    -- (--uninstall | --hostname <NAME>)
+    "github:incremental-design/projects?dir=infrastructure#install" \
+    --extra-experimental-features \
+    "nix-command flakes" \
+    -- system \
+    --hostname <hostname>
+    ```
+
+    | arg | value | example |
+    |:----|:------|:--------|
+    |`<hostname>` | hostname of the system | "my-computer" |
+
     EOF
-      exit 0
     fi
 
-    if [ -z "$HOSTNAME" ] && [ -z "$UNINSTALL" ]; then
-        echo "you must set a --hostname" >&2
-        exit 1
+    if (( HELPMSG == 2)) || (( HELPMSG == 3 )); then
+    glow >&2 <<'EOF'
+    # INSTALL Home Manager for currently logged in user
+
+    ```
+    nix run \
+    "github:incremental-design/projects?dir=infrastructure#install" \
+    --extra-experimental-features \
+    "nix-command flakes" \
+    -- home
+    ```
+
+    EOF
     fi
 
-    backup_darwin_flake(){
-        if ! { rm -f "/var/root/flake.lock" && mv "/var/root/flake.nix" "/var/root/flake.nix.old"; }; then
-            return 1
-        fi
-    }
+    if (( HELPMSG == 4 )) || (( HELPMSG == 3 )); then
+    glow >&2 <<'EOF'
+    # UNINSTALL NixOS or nix-darwin on MacOS
 
-    backup_darwin_flake_uninstall(){
-        if ! backup_darwin_flake; then
-            echo "could not back up \"/var/root/flake.nix\" to \"/var/root/flake.nix.old\". Not proceeding with uninstallation" >&2
-            EXIT=1
-            CMD=""
-        else
-            CMD="uninstall_macos"
-        fi
-    }
+    ```
+    sudo -H nix run \
+    "github:incremental-design/projects?dir=infrastructure#install" \
+    --extra-experimental-features \
+    "nix-command flakes" \
+    -- uninstall-system
+    ```
 
-    nix_darwin_uninstall(){
-        if ! nix run nix-darwin#darwin-uninstaller; then
-          echo "could not \"nix run nix-darwin#darwin-uninstaller\". Not uninstalling nix-darwin. See https://github.com/nix-darwin/nix-darwin#uninstalling for instructions on how to uninstall manually" >&2
-          EXIT=1
-          CMD=""
-        else
-          CMD="backup_darwin_flake_uninstall"
-        fi
-    }
+    EOF
+    fi
 
-    uninstall_macos(){
-        if ! [ -f "/var/root/flake.nix" ]; then
-            echo "uninstall complete" >&2
-            CMD=""
-        else
-            CMD="nix_darwin_uninstall"
-        fi
-    }
+    if (( HELPMSG == 5)) || (( HELPMSG == 3 )); then
+    glow >&2 <<'EOF'
+    # UNINSTALL Home Manager for currently logged in user
 
+    ```
+    nix run \
+    "github:incremental-design/projects?dir=infrastructure#install" \
+    --extra-experimental-features \
+    "nix-command flakes" \
+    -- uninstall-home
+    ```
 
-    backup_darwin_flake_install(){
-        if ! backup_darwin_flake; then
-            echo "could not back up \"/var/root/flake.nix\" to \"/var/root/flake.nix.old\". Not proceeding with installation" >&2
-            EXIT=1
-            CMD=""
-        else
-            CMD="install_macos"
-        fi
-    }
+    EOF
+    fi
 
-    restore_darwin_flake(){
-        if ! mv "/var/root/flake.nix.old" "/var/root/flake.nix"; then
-            echo "could not restore \"/var/root/flake.nix.old\" to \"/var/root/flake.nix\"" >&2
-            EXIT=1
-        else
-            echo "restored /var/root/flake.nix" >&2
-        fi
-        CMD=""
-    }
+    if [ "$SUBCOMMAND" = "system" ] && [ "$EUID" -ne 0 ]; then
+        echo "run install system with sudo -H" >&2
+        EXIT_CODE=1
+        exit "$EXIT_CODE"
+    fi
 
-    nix_darwin_switch(){
-        if ! nix run \
-          --extra-experimental-features "nix-command flakes" \
-          nix-darwin/nix-darwin-26.05#darwin-rebuild \
-          -- \
-          switch \
-          --flake "/var/root/.#default" \
-          --show-trace
-        then
-            echo "failed to \"nix run nix-darwin/nix-darwin-26.05#darwin-rebuild -- switch\"" >&2
-            CMD="restore_darwin_flake"
-        fi
-        # todo, this terminates with 0 but later we will want to kick things over to home manager
-        CMD=""
-    }
+    if [ "$SUBCOMMAND" = "uninstall-system" ] && [ "$EUID" -ne 0 ]; then
+        echo "run uninstall-system with sudo -H" >&2
+        EXIT_CODE=1
+        exit "$EXIT_CODE"
+    fi
 
-    init_darwin_flake(){
-
-        local macos_system_arch
-
-        if ! macos_system_arch=$(uname -m); then
-            echo "failed to get system architecture with \"uname -m\"" >&2
-            EXIT=1
-            CMD=""
-            return
-        fi
-
-        if [ "$macos_system_arch" = "arm64" ]; then
-            macos_system_arch='macos_system_arch = "aarch64-darwin";'
-        else
-            macos_system_arch='macos_system_arch = "x86_64-darwin";'
-        fi
-
-        if nix flake init -t "github:incremental-design/projects?dir=infrastructure#macos" && \
-           sed -i "s|macos_system_arch = \"aarch64-darwin\";|''${macos_system_arch}|g" "/var/root/flake.nix" && \
-           sed -i "s|# networking.hostName|networking.hostName = \"''${HOSTNAME}\";|g" "/var/root/flake.nix";
-        then
-            CMD="nix_darwin_switch"
-        else
-            echo "failed to run \"nix flake init -t 'github:incremental-design/projects?dir=infrastructure#macos'\"" >&2
-            EXIT=1
-            if [ -f "/var/root/flake.nix.old" ]; then
-                CMD="restore_darwin_flake"
-            else
-                CMD=""
-            fi
-        fi
-    }
-
-    install_macos(){
-        if ! [ "$HOME" = "/var/root" ]; then
-            echo "home directory must be /var/root, try again with sudo -H" >&2
-            EXIT=1
-            CMD=""
-        elif ! [ -f "/var/root/flake.nix" ]; then
-            CMD="init_darwin_flake"
-        else
-            CMD="backup_darwin_flake_install"
-        fi
-    }
-
-    # uninstall_nixos(){
-    #     echo "err not implemented" >&2
-    #     EXIT=1
-    # }
-    # install_nixos(){
-    #     echo "err not implemented" >&2
-    #     EXIT=1
-    # }
+    if (( EXIT_CODE != 0 )) || (( HELPMSG != 0 )); then
+        exit "$EXIT_CODE"
+    fi
 
     (
-      CMD="install_nixos"
-      EXIT=0
+        cd "$HOME"
 
-      if [ "$(uname)" = "Darwin" ] && [ -z "$UNINSTALL" ]; then
-      CMD="install_macos"
-      elif [ "$(uname)" = "Darwin" ]; then
-      CMD="uninstall_macos"
-      elif [ -z "$UNINSTALL" ]; then
-      CMD="uninstall_nixos"
-      fi
+        if [ "$SUBCOMMAND" = "system" ]; then
 
-      if ! cd /var/root; then
-          echo "cannot cd to /var/root, does it exist" >&2
-          EXIT=1
-          CMD=""
-      fi
+            if [ -f "./flake.nix" ] && ! mv "./flake.nix" "./flake.nix.old"; then
+                echo "could not back up ''${PWD}/flake.nix to ''${PWD}/flake.nix.old" >&2
+                exit 1
+            fi
 
-      while [ -n "$CMD" ]; do
-          case "$CMD" in
-              "nix_darwin_switch") nix_darwin_switch;;
-              "backup_darwin_flake_uninstall") backup_darwin_flake_uninstall;;
-              "backup_darwin_flake_install") backup_darwin_flake_install;;
-              "restore_darwin_flake") restore_darwin_flake;;
-              "init_darwin_flake") init_darwin_flake;;
-              "install_macos") install_macos;;
-              "nix_darwin_uninstall") nix_darwin_uninstall;;
-              "uninstall_macos") uninstall_macos;;
-              *) echo "unrecognized command" >&2; exit 1;;
-          esac
-      done
+            MACOS_SYSTEM_ARCH="aarch64-darwin"
 
-      exit "$EXIT"
+            if ! [ "$(uname -m)" = "arm64" ]; then
+                MACOS_SYSTEM_ARCH="x86_64-darwin"
+            fi
+
+            if ! nix flake init -t "github:incremental-design/projects?dir=infrastructure#macos" && \
+                sed -i "s|macos_system_arch = \"aarch64-darwin\";|''${MACOS_SYSTEM_ARCH}|g" "/var/root/flake.nix" && \
+                sed -i "s|# networking.hostName|networking.hostName = \"''${SYSTEM_HOSTNAME}\";|g" "/var/root/flake.nix";
+            then
+                echo "could not init \"https://github.com/incremental-design/projects/blob/main/infrastructure/macos/system/template/flake.nix\" into ''${PWD}/flake.nix" >&2
+
+                if ! mv "''${PWD}/flake.nix.old" "''${PWD}/flake.nix"; then
+                echo "could not restore ''${PWD}/flake.nix.old to ''${PWD}/flake.nix" >&2
+                fi
+
+                exit 1
+            fi
+
+            if ! nix run \
+                --extra-experimental-features "nix-command flakes" \
+                nix-darwin/nix-darwin-26.05#darwin-rebuild \
+                -- \
+                switch \
+                --flake .#default \
+                --show-trace
+            then
+                echo "failed to \`nix run nix-darwin/nix-darwin-26.05#darwin-rebuild -- switch\`" >&2
+
+                if ! mv "''${PWD}/flake.nix.old" "''${PWD}/flake.nix"; then
+                echo "could not restore ''${PWD}/flake.nix.old to ''${PWD}/flake.nix" >&2
+                fi
+
+                exit 1
+            fi
+
+        elif [ "$SUBCOMMAND" = "uninstall-system" ]; then
+
+            if ! nix run nix-darwin/nix-darwin-26.05#darwin-uninstaller; then
+                echo "faild to \`nix run nix-darwin/nix-darwin-26.05#darwin-uninstaller\`" >&2
+            fi
+
+        elif [ "$SUBCOMMAND" = "home" ]; then
+            echo "not implemented" >&2
+            exit 1
+        elif [ "$SUBCOMMAND" = "uninstall-home" ]; then
+            echo "not implemented" >&2
+            exit 1
+        fi
     )
+
   '';
 }
