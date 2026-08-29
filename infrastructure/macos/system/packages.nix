ac4079809920d7d34314c8fe336ee17fecf5fd33
Ajay Ganapathy <ajay@incremental.design>
2026-08-29 12:26:22 -0400

    chore: remove nushell plugins from system flake
    Signed-off-by: Ajay Ganapathy <ajay@incremental.design>

--- infrastructure/macos/system/packages.nix
+++ infrastructure/macos/system/packages.nix
@@ -13,22 +13,12 @@
     /*
     all pkgs
     */
-    git
     starship
     nix-direnv
     /*
     nushell
     */
     nushell
-    nushell-plugin-bson
-    nushell-plugin-formats
-    nushell-plugin-gstat
-    nushell-plugin-hcl
-    nushell-plugin-highlight
-    nushell-plugin-polars
-    nushell-plugin-query
-    nushell-plugin-semver
-    nushell-plugin-skim
     /*
     packages for zsh, bash
     */
