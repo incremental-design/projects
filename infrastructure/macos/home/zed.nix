e78301e1c57e448d7cf9498fcb520ecaf7eae514
Ajay Ganapathy <ajay@incremental.design>
2026-08-30 19:33:13 -0400

    feat: set nu as zed terminal
    Signed-off-by: Ajay Ganapathy <ajay@incremental.design>

--- infrastructure/macos/home/zed.nix
+++ infrastructure/macos/home/zed.nix
@@ -1,10 +1,21 @@
-{...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  nuPkg = if config.programs.nushell.package != null then config.programs.nushell.package else pkgs.nushell;
+in {
   programs.zed-editor = {
     enable = true;
     package = null; # don't install the zed editor for user, just patch config file
     userSettings = {
       buffer_font_family = "Lilex Nerd Font Propo";
       terminal.font_family = "Lilex Nerd Font Propo";
+      terminal.shell.with_arguments = {
+        program = lib.getExe nuPkg;
+        args = ["--login"];
+      };
     };
   };
 }
